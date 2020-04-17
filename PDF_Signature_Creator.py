cac032912becba05fe402ee935ce028cd44e5244
admire-brandon-0206 <49301904+admire-brandon-0206@users.noreply.github.com>
2020-04-17 15:42:18 -0700

    Add files via upload

--- PDF_Signature_Creator.py
+++ PDF_Signature_Creator.py
@@ -14,10 +14,8 @@ This program has the following optional arguments
 import sys, getopt, PyPDF2
 from math import ceil
 
-def main(argv):
-    """Driver code for program."""
 
-    # Parse arguments
+def getUserInput(argv):
     input_file = ''
     output_file = './output.pdf'
     sig_num = 0
@@ -30,14 +28,18 @@ def main(argv):
         print("Not enough arguments! -h for help.")
     for opt, arg in opts:
         if opt == '-h':
-            print("--PDF SIGNATURE CREATOR--\n\n\
-This program takes a PDF as input and rearranges and outputs the pdf into\n\
-signatures for bookbinding purposes. Blank pages are added at the end of the\n\
-document to create even signatures.\n\n\
-This program has the following optional arguments\n\
--i : input \n\
--o : output path \n\
--n : Number of signatures to split document into.")
+            print(
+                """
+                --PDF SIGNATURE CREATOR--
+                This program takes a PDF as input and rearranges and outputs the pdf into\n\
+                signatures for bookbinding purposes. Blank pages are added at the end of the\n\
+                document to create even signatures.
+                This program has the following optional arguments
+                -i : input 
+                -o : output path
+                -n : Number of signatures to split document into.
+                """
+            )
             sys.exit()
         elif opt == '-i':
             input_file = arg
@@ -49,29 +51,38 @@ This program has the following optional arguments\n\
             except ValueError:
                 print("The number of signatures must be a whole number.")
                 sys.exit(2)
-
-    # Initialize the PyPDF2 objects
-    pdf = PyPDF2.PdfFileReader(input_file)
-    output = PyPDF2.PdfFileWriter()
-
-    # Calculate pages per signature
-    pages = ceil(pdf.getNumPages()/sig_num)
-    # Round up to the nearest multiple of 4
-    pages = int(4 * ceil(pages/4))
-
-    # Get dimensions for filler page
-    _,_, w, h = pdf.getPage(1)['/MediaBox']
     
-    # Prepare output doc
+    return input_file,output_file,sig_num
+
+
+def pageCalc(sig_num,pdf_object):
+    """Calculate and return pages per signature."""
+
+    pages = ceil(pdf_object.getNumPages()/sig_num)
+    # Round up to the nearest multiple of 4. All new pages will consist of 
+    # 4 of the original, 2 on each side. So the signatures need to be split into
+    # exact multiples of 4.
+    pages = int(4 * ceil(pages/4))
+    # Print out
+    print("")
+    return pages
+
+
+def buildOutput(pdf_object,output_object, pages, sig_num):
+    """Build output for write"""
+
+    # Get dimensions for filler pages
+    _,_, w, h = pdf_object.getPage(1)['/MediaBox']
+
     for j in range(sig_num):
         for i in range(int(pages/2)):
             # Get original pages
             try:
-                left = pdf.getPage((i)+(j*pages))
+                left = pdf_object.getPage((i)+(j*pages))
             except IndexError:
                 left = PyPDF2.pdf.PageObject.createBlankPage(width =w,height=h)
             try:
-                right = pdf.getPage((pages-i)+(j*pages))
+                right = pdf_object.getPage((pages-i)+(j*pages))
             except IndexError:
                 right = PyPDF2.pdf.PageObject.createBlankPage(width =w,height=h)
 
@@ -80,12 +91,24 @@ This program has the following optional arguments\n\
             left.mergeTranslatedPage(right, left.mediaBox.getUpperRight_x(),0, True)
 
             # Add page to output
-            output.addPage(left)
+            output_object.addPage(left)
 
 
-    # Write output
-    with open(output_file, "wb") as target:
+def main(argv):
+    in_path, out_path, sig_num = getUserInput(argv)
+    print("Program started....")
+    # Initialize the PyPDF2 objects
+    pdf = PyPDF2.PdfFileReader(in_path)
+    output = PyPDF2.PdfFileWriter()
+
+    pages = pageCalc(sig_num, pdf)
+    print(f"There will be {pages} per signature!")
+    buildOutput(pdf,output,pages,sig_num)
+
+    with open(out_path, "wb") as target:
         output.write(target)
+    print(f"Job complete! File saved to: {out_path}")
+
 
 
 if __name__ == "__main__":
